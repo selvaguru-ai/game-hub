78f0e358b66adccce1ac7f02754143393f5dd5bf
selvaguru <selvaguru08@gmail.com>
2024-09-01 13:16:13 -0500

    Refractor Remove Duplicated Styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="250px" borderRadius={10}>
+    <Card>
       <Skeleton height="250px" />
       <CardBody>
         <SkeletonText />
